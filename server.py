48c62d6832a2f33a3b670dca700e8efe5c6a4e44
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-01-14 13:07:00 +0100

    Registration / login form added.

--- server.py
+++ server.py
@@ -200,19 +200,6 @@ def route_new_answer_comment(answer_id):
                                answer_comment=answer_comment, answer_id=answer_id)
 
 
-@app.route("/answer/<answer_id>/new_comment", methods=["GET", "POST"])
-def route_new_answer_comment(answer_id):
-    question_id = data_manager.get_question_id_from_answer(answer_id)
-    if request.method == 'POST':
-        item_data = {"message": request.form["message"], "answer_id": answer_id}
-        data_manager.insert_new_answer_comment(item_data)
-        return redirect(url_for("route_question", question_id=question_id))
-    else:
-        add_comment_answer = True
-        return render_template('form.html', title="Add a comment", answer_id=answer_id,
-                               add_comment_answer=add_comment_answer)
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
             port=8000,
